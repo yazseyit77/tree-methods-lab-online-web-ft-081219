04bac67b66789a313cdaeed459a4f9adaf165f55
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-09 20:07:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function inOrder(rootNode){
+  if (rootNode.left){
+    inOrder(rootNode.left);
+  };
+
+  console.log(rootNode.data);
+
+  if (rootNode.right){
+    inOrder(rootNode.right);
+  };
+};
+
+function findOrAdd(rootNode, newNode){
+  if (rootNode.data === newNode.data){
+    return true;
+  } else if (rootNode.data > newNode.data) {
+    if (rootNode.left){
+      return findOrAdd(rootNode.left, newNode);
+    } else {
+      return rootNode.left = newNode;
+    };
+  } else {
+    if(rootNode.right){
+      return findOrAdd(rootNode.right, newNode);
+    } else {
+      return rootNode.right = newNode;
+    };
+  };
+};
+
+
+
+function max(rootNode){
+  let right = rootNode.right;
+  if(right){
+    return max(right);
+  } else {
+    return rootNode;
+  };
+};
+
+function min(rootNode){
+  let left = rootNode.left;
+  if(left){
+    return min(left);
+  }else{
+    return rootNode;
+  };
+};
