bb683a146acd5ecfcd066e104713397be48fb400
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-09 20:01:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function inOrder(rootNode){
   if (rootNode.left){
-    inOrder(rootNode.left)
+    inOrder(rootNode.left);
+  };
+
+  console.log(rootNode.data);
+
+  if (rootNode.right){
+    inOrder(rootNode.right)
   }
 }
